7dddf2a3a19b85015d68368d32668cfd2065a54d
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-01-29 16:07:17 +0100

    fix (door-policy exo): correction de l'exercice

--- exercices/poetry-club-door-policy/door-policy.js
+++ exercices/poetry-club-door-policy/door-policy.js
@@ -38,10 +38,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  let firstpartword = word[0].toUpperCase();
-  let lastpartword = word.slice(1).toLowerCase();
-  let entireWord = firstpartword.concat(lastpartword);
-  return entireWord;
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 /**
@@ -52,9 +49,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  line = line.trim();
-  let lastChar = line[line.length - 1];
-  return lastChar;
+  line = line.trim() 
+  return line[line.length-1];
 }
 
 /**
@@ -65,10 +61,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  word = word.trim();
-  let firstpartword = word[0].toUpperCase();
-  let lastpartword = word.slice(1).toLowerCase();
-  let entireWord = firstpartword.concat(lastpartword);
-  let phrase = entireWord.concat(", please");
-  return phrase;
+  return frontDoorPassword(word) + ", please";
 }
