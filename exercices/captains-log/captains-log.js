44939d54f943413803ef150b0a85bacc914d4863
Kentin Fankhauser <pm27rlp@eduvaud.ch>
2026-02-05 16:26:44 +0100

    chore(captain log): exercice fini [20] [DONE]

--- exercices/captains-log/captains-log.js
+++ exercices/captains-log/captains-log.js
@@ -2,24 +2,30 @@
 
 /**
  * Generates a random starship registry number.
- *
+ * @param {const} minLimit
+ * @param {const} maxLimit
  * @returns {string} the generated registry number.
  */
-export function randomShipRegistryNumber() {
-  throw new Error(
-    'Please remove this line and implement the randomShipRegistryNumber() function',
-  );
+export function randomShipRegistryNumber(minLimit,maxLimit) {
+  minLimit = 1000;
+  maxLimit = 9999;
+  let random;
+  random = Math.random() *(maxLimit-minLimit) + minLimit;
+  return "NCC-"+random;
 }
 
 /**
  * Generates a random stardate.
- *
+ * @param {const} minLimit
+ * @param {const} maxLimit
  * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
  */
-export function randomStardate() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+export function randomStardate(minLimit,maxLimit) {
+  minLimit = 41000.0
+  maxLimit = 42000.0
+  let random;
+  random = Math.random() *(maxLimit - minLimit) + minLimit
+  return random;
 }
 
 /**
@@ -28,7 +34,7 @@ export function randomStardate() {
  * @returns {string} a one-letter planet class.
  */
 export function randomPlanetClass() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  const letters = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"];
+  const index = Math.floor(Math.random() * letters.length);
+  return letters[index];
 }
